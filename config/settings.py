50279607add25c8390e07aa5f229d1f8729b9659
Andy <andy@andys-mbp.lan>
2021-02-01 19:24:41 +0000

    new host

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f&rjjbx7u4pmfch=1(_cy+0f8gsg*7c5j)ho+l1k1g_=_+mcns'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
